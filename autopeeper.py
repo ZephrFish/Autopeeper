9126054ac4bc080b407a947cb6a4fde8431d5dd8
ZephrFish <justzero112@gmail.com>
2016-03-13 16:30:54 +0000

    Checking of args added to script

--- autopeeper.py
+++ autopeeper.py
@@ -1,11 +1,28 @@
-# Autopeeper v1.0
+# Autopeeper v0.2
 # Automated Web Application Screenshotter
 # ZephrFish 2016
+import sys
 from sys import argv
+import os
+import subprocess
 import argparse
 
 # Define basic variables
 #usage = """
+
+# Check if user has supplied flags
+if len(sys.argv) != 2:
+  print """ Usage: python autopeeper.py [-h] [-f FILE] [-d] [-v VERBOSE] [-o OUTPUT DIRECTORY]
+		
+	optional arguments: 
+		-h, --help show this help message and exit 
+		-f FILE targets file, file containing target urls 
+		-d debugging mode, turns on debugging 
+		-v VERBOSE verbose mode 
+		-o OUTPUT DIRECTORY output directory
+		"""
+  print
+  sys.exit()
 
 # Usage Information
 parser = argparse.ArgumentParser()
@@ -16,8 +33,7 @@ parser.add_argument('-o', help='output directory', dest='output directory', acti
 
 opts = parser.parse_args()
 
-# Required arguments for file
-script, file = argv
+script, options, file = argv
 
 # Start to parse out target urls
 
@@ -25,7 +41,16 @@ if opts.file:
 	try:
 	     targets = open(opts.file).read().split()
 	except IOError:
-	     print '[!!] Invalid File, or incorrect path to %r ' % opts.file
+	     print '[!] Invalid File, or incorrect path to %r ' % opts.file
+
+
+# Scanning Function, web, rpc, vnc and more
+def scanning():
+	web = [80,443,8080,8000,9000,8081,8082,9090,8000]
+	rpcd = [3389]
+	vnc = [5900]
+	others = []
+
 
 
 # Set Targets as file provided to script
