ba5af41ce7bf48daeb38cc924bc3a588cce72466
ZephrFish <ZephrFish@users.noreply.github.com>
2016-03-18 20:30:23 +0000

    Update autopeeper.py

--- autopeeper.py
+++ autopeeper.py
@@ -1,70 +1,81 @@
-# Autopeeper v0.2
+# Autopeeper v0.21
+# Working Copy
 # Automated Web Application Screenshotter
 # ZephrFish 2016
 import sys
-from sys import argv
 import os
 import subprocess
 import argparse
+import commands
+from argparse
+import ArgumentParser
 
-# Define basic variables
-#usage = """
+# File & Output Path
+file = None
+outdir = None
 
-# Check if user has supplied flags
-if len(sys.argv) != 2:
-  print """ Usage: python autopeeper.py [-h] [-f FILE] [-d] [-v VERBOSE] [-o OUTPUT DIRECTORY]
-		
-	optional arguments: 
-		-h, --help show this help message and exit 
-		-f FILE targets file, file containing target urls 
-		-d debugging mode, turns on debugging 
-		-v VERBOSE verbose mode 
-		-o OUTPUT DIRECTORY output directory
-		"""
-  print
-  sys.exit()
+# Autopeeper Logo Echo
+print('''
+                _                                                 ___   ___  
+     /\        | |                                               / _ \ |__ \ 
+    /  \  _   _| |_ ___  _ __   ___  ___ _ __   ___ _ __  __   _| | | |   ) |
+   / /\ \| | | | __/ _ \| '_ \ / _ \/ _ \ '_ \ / _ \ '__| \ \ / / | | |  / / 
+  / ____ \ |_| | || (_) | |_) |  __/  __/ |_) |  __/ |     \ V /| |_| | / /_ 
+ /_/    \_\__,_|\__\___/| .__/ \___|\___| .__/ \___|_|      \_/  \___(_)____|
+                        | |             | |                                  
+  ______          _     |_|  ______ _   |_|                                  
+ |___  /         | |        |  ____(_)   | |                                 
+    / / ___ _ __ | |__  _ __| |__   _ ___| |__                               
+   / / / _ \ '_ \| '_ \| '__|  __| | / __| '_ \                              
+  / /_|  __/ |_) | | | | |  | |    | \__ \ | | |                             
+ /_____\___| .__/|_| |_|_|  |_|    |_|___/_| |_|                             
+           | |                                                               
+           |_|                                                               
+''')
 
-# Usage Information
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', help='targets file, file containing target urls', dest='file', action='store')
-parser.add_argument('-d', help='debugging mode, turns on debugging', dest='debugging', action='store_true')
-parser.add_argument('-v', help='verbose mode', dest='verbose', action='store')
-parser.add_argument('-o', help='output directory', dest='output directory', action='store')
+# Defining Functions
 
-opts = parser.parse_args()
-
-script, options, file = argv
-
-# Start to parse out target urls
-
-if opts.file:
-	try:
-	     targets = open(opts.file).read().split()
-	except IOError:
-	     print '[!] Invalid File, or incorrect path to %r ' % opts.file
-
-
-# Scanning Function, web, rpc, vnc and more
-def scanning():
-	web = [80,443,8080,8000,9000,8081,8082,9090,8000]
-	rpcd = [3389]
-	vnc = [5900]
-	others = []
-
-
-
-# Set Targets as file provided to script
-targets = open(file)
-num_lines = sum(1 for line in open(file))
-
-# Debugging step, prints out the hosts from file
+# Debugging Function
 def debugging():
-	print "The output from %r is shown: " % file	
-	print targets.read()
-	print "There are a total of %r targets in the file provided " % num_lines
+		if args.debugging: 
+			print('[+] Debugging [Enabled] Disabled');
+			targets = open(file);
+		num_lines = sum(1 for line in open(file));
+        print "The output from %r is shown: " % file;
+        print targets.read();
+        print "There are a total of %r targets in the file provided " % num_lines;
+		else:
+			return print('[-] Debugging Enabled [Disabled]');
 
+def verbose():
+		if args.verbose:
+			
+			
+			
+# Main method, to check input parameters
+if __name__ == "__main__":
+   
+        parser = ArgumentParser(description="Tool for Screenshotting Web Applications Automatically")
 
+        
+        parser.add_argument("file", help="File to check version of")
+        parser.add_argument("outdir", help="Directory of local git repository")
 
-# Run Main Code
-# Uncomment line below to run debugging
-#debugging()
+        
+        parser.add_argument("-v", "--verbose", action="store_true",help="verbose mode")
+        parser.add_argument("-d", "--debugging", action="store_true", help='debugging mode, turns on debugging', dest='debugging')
+
+        
+        args = parser.parse_args()
+		
+		
+		if os.path.isfile(args.file)==False:
+			parser.print_help()
+			print "[!] Targets File does not exist [!]"
+			sys.exit(-1)
+		
+		# check that the directory exists
+		#if os.path.isdir(args.outdir)==False:
+		#	parser.print_help()
+		#	print "[!] Output directory does not exist [!]"
+		#	sys.exit(-1)
